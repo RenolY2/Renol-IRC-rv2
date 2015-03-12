4e68fa7358248c7070d2a7118a4de18f9ab325b9
Yoshi2 <debugyoshi@gmail.com>
2015-03-12 22:36:58 +0100

    Add lower case translation of name with reference to RFC

--- core/irc/irc_target.py
+++ core/irc/irc_target.py
@@ -1,6 +1,32 @@
+
 # Class that covers some aspects used by both an IRC user and an IRC channel.
 
+# According to the RFC 1459, {,} and | are the lower case variants of
+# the characters [,] and /, respectively.
+LOWERCASE_TRANS = str.maketrans("[]\\", "{}|")
+UPPERCASE_TRANS = str.maketrans("{}|", "[]\\")
 
-class IRCItem(object):
-    def __init__(self):
-        pass
+
+class IRC_Target(object):
+    def __init__(self, bot_cls, name):
+        self._bot_cls = bot_cls
+
+        self.name = name
+
+    @property
+    def name_lowercase(self):
+        name_lower = self.name.lower()
+        return name_lower.translate(LOWERCASE_TRANS)
+
+
+
+
+
+if __name__ == "__main__":
+    user1 = IRC_Target(None, "HansMaus12312")
+    print(user1.name_lowercase)
+    user2 = IRC_Target(None, "IamNotHere[GONE]")
+    print(user2.name_lowercase)
+
+    channel = IRC_Target(None, "#BÄSSSß\\")
+    print(channel.name_lowercase)
