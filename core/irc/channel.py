97629e079c8787261f5bb9c2d665b3226ae67e74
Yoshi2 <debugyoshi@gmail.com>
2015-03-12 21:26:09 +0100

    Added more files

---
+++ core/irc/channel.py
@@ -0,0 +1,4 @@
+
+
+class Channel(object):
+    pass
---
+++ core/irc/irc_target.py
@@ -0,0 +1,6 @@
+# Class that covers some aspects used by both an IRC user and an IRC channel.
+
+
+class IRCItem(object):
+    def __init__(self):
+        pass
