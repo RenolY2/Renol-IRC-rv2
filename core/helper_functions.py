31e56205b8814356d19b2eaade2f38748a2a23aa
Yoshi2 <debugyoshi@gmail.com>
2015-03-13 12:29:41 +0100

    Add less strict rfc1459 casemapping

--- core/helper_functions.py
+++ core/helper_functions.py
@@ -2,9 +2,22 @@
 
 # According to the RFC 1459, {,} and | are the lower case variants of
 # the characters [,] and /, respectively.
-LOWERCASE_TRANS = str.maketrans("[]\\", "{}|")
-UPPERCASE_TRANS = str.maketrans("{}|", "[]\\")
+STRICT_LOWERCASE_TRANS = str.maketrans("[]\\", "{}|")
+STRICT_UPPERCASE_TRANS = str.maketrans("{}|", "[]\\")
 
+# According to http://www.irc.org/tech_docs/005.html there is a less strict
+# variant of the casemapping that considers ~ to be the lower case variant of ^.
+LOWERCASE_TRANS = str.maketrans("[]\\^", "{}|~")
+UPPERCASE_TRANS = str.maketrans("{}|~", "[]\\^")
+
+
+
+def lowercase_strict_rfc1459(name):
+    name_lower = name.lower()
+    return name_lower.translate(STRICT_LOWERCASE_TRANS)
+def uppercase_strict_rfc1459(name):
+    name_upper = name.upper()
+    return name_upper.translate(STRICT_UPPERCASE_TRANS)
 
 def lowercase_rfc1459(name):
     name_lower = name.lower()
@@ -12,6 +25,7 @@ def lowercase_rfc1459(name):
 def uppercase_rfc1459(name):
     name_upper = name.upper()
     return name_upper.translate(UPPERCASE_TRANS)
+
 
 def uppercase_default(name):
     return name.upper()
@@ -23,7 +37,9 @@ def choose_casemapping_func(casemap):
     casemap_funcs = {"rfc1459": (lowercase_rfc1459,
                                   uppercase_rfc1459),
                      "default": (lowercase_default,
-                                uppercase_default)}
+                                uppercase_default),
+                     "strictrfc1459" : (lowercase_strict_rfc1459,
+                                        uppercase_strict_rfc1459)}
 
     if casemap in casemap_funcs:
         return casemap_funcs[casemap]
@@ -33,8 +49,11 @@ def choose_casemapping_func(casemap):
 if __name__ == "__main__":
     lowercase, uppercase = choose_casemapping_func("rfc1459")
 
-    print(lowercase("Bla[]"))
-    print(uppercase("Bla[]"))
+    print(lowercase("Bla[]^"))
+    print(uppercase("Bla[]~"))
+
+    lowercase, uppercase = choose_casemapping_func("strictrfc1459")
+    print(lowercase("BLA[]^^"))
 
 
     lowercase, uppercase = choose_casemapping_func("none")
