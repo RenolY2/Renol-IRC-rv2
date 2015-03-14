87fd3ab7b77c0a830e1287077cf51d43e001bb75
Yoshi2 <debugyoshi@gmail.com>
2015-03-14 11:44:42 +0100

    Improved code; No classes in classes

--- core/irc/irc_channel.py
+++ core/irc/irc_channel.py
@@ -1,49 +1,53 @@
 from irc_target import IRC_Target
 from collections import MutableMapping
 
+class NormalizedDict(MutableMapping):
+    def __init__(self, normalize_func, *args, **kwargs):
+        self.dict = dict()
+        self.update(dict(*args, **kwargs))
+        self.norm_func = normalize_func
+
+    def __getitem__(self, item):
+        self.dict[self.norm_func(item)]
+    def __setitem__(self, key, value):
+        self.dict[self.norm_func(key)] = value
+    def __delitem__(self, key):
+        del self.dict[self.norm_func(key)]
+    def __contains__(self, item):
+        return self.norm_func(item) in self.dict
+    def __iter__(self):
+        return iter(self.dict)
+    def __len__(self):
+        return len(self.dict)
+    def __str__(self):
+        return str(self.dict)
+    def __repr__(self):
+        return repr(self.dict)
+
+
+class Channel(IRC_Target):
+    topic = ""
+    userlist = None
+
+
+    def __init__(self, lowercase_func, bot_cls, name):
+        super(Channel, self).__init__(bot_cls, name)
+        self.userlist = NormalizedDict(lowercase_func)
+
+    def _set_topic(self, topic):
+        pass
+
 
 class ChannelFactory(object):
     def __init__(self, lowercase):
         self.lowercase = lowercase
 
-    def create_channel(self_, bot_cls, name):
-
-        class NormalizedDict(MutableMapping):
-            def __init__(self, *args, **kwargs):
-                self.dict = dict()
-                self.update(dict(*args, **kwargs))
-            def __getitem__(self, item):
-                self.dict[self_.lowercase(item)]
-            def __setitem__(self, key, value):
-                self.dict[self_.lowercase(key)] = value
-            def __delitem__(self, key):
-                del self.dict[self_.lowercase(key)]
-            def __contains__(self, item):
-                return self_.lowercase(item) in self.dict
-            def __iter__(self):
-                return iter(self.dict)
-            def __len__(self):
-                return len(self.dict)
-            def __str__(self):
-                return str(self.dict)
-            def __repr__(self):
-                return repr(self.dict)
+    def create_channel(self, bot_cls, name):
+        return Channel(self.lowercase, bot_cls, name)
 
 
 
-        class Channel(IRC_Target):
-            topic = ""
-            userlist = NormalizedDict()
 
-
-            def __init__(self_, bot_cls, name):
-                super(Channel, self_).__init__(bot_cls, name)
-
-
-            def _set_topic(self_, topic):
-                pass
-
-        return Channel(bot_cls, name)
 
 if __name__ == "__main__":
     def lowercase(str):
