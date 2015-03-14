001db0597a92674b75553bbd6f7a4466008bc7c9
Yoshi2 <debugyoshi@gmail.com>
2015-03-14 11:16:29 +0100

    Add channel factory that uses a special dict for userlist

--- core/irc/irc_channel.py
+++ core/irc/irc_channel.py
@@ -1,10 +1,58 @@
 from irc_target import IRC_Target
-
-class Channel(IRC_Target):
-    topic = ""
-    def __init__(self, bot_cls, name):
-        super(Channel, self).__init__(bot_cls, name)
+from collections import MutableMapping
 
 
-    def _set_topic(self, topic):
-        pass
+class ChannelFactory(object):
+    def __init__(self, lowercase):
+        self.lowercase = lowercase
+
+    def create_channel(self_, bot_cls, name):
+
+        class NormalizedDict(MutableMapping):
+            def __init__(self, *args, **kwargs):
+                self.dict = dict()
+                self.update(dict(*args, **kwargs))
+            def __getitem__(self, item):
+                self.dict[self_.lowercase(item)]
+            def __setitem__(self, key, value):
+                self.dict[self_.lowercase(key)] = value
+            def __delitem__(self, key):
+                del self.dict[self_.lowercase(key)]
+            def __contains__(self, item):
+                return self_.lowercase(item) in self.dict
+            def __iter__(self):
+                return iter(self.dict)
+            def __len__(self):
+                return len(self.dict)
+            def __str__(self):
+                return str(self.dict)
+            def __repr__(self):
+                return repr(self.dict)
+
+
+
+        class Channel(IRC_Target):
+            topic = ""
+            userlist = NormalizedDict()
+
+
+            def __init__(self_, bot_cls, name):
+                super(Channel, self_).__init__(bot_cls, name)
+
+
+            def _set_topic(self_, topic):
+                pass
+
+        return Channel(bot_cls, name)
+
+if __name__ == "__main__":
+    def lowercase(str):
+        return str.lower()
+
+    fact = ChannelFactory(lowercase)
+    chan = fact.create_channel(None, "Hello")
+
+    dic = chan.userlist
+    dic["HELLO"] = "hi"
+    print(dic)
+    print("hElLo" in dic)
