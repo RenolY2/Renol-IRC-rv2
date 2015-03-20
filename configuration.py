73d83d067eb9f1a562859e223e29bc5b3d4ca5c3
Yoshi2 <debugyoshi@gmail.com>
2015-03-20 22:39:30 +0100

    Change Config class to container for config data
    Add methods that make it possible to use the config object as a dict.
    Filepath of config file is now defined on object init.

--- configuration.py
+++ configuration.py
@@ -1,31 +1,51 @@
 import yaml
+import os
 
-from collections import MutableMapping
+from collections import MutableMapping, OrderedDict
 
 from misc.yaml_additions import Ordered_Loader, Ordered_Dumper
 
 # Through the Config class, it should be possible to define
 # a basic schema for validating a config file that should be
 # loaded. The schema should also double as a default configuration.
-class ConfigSchema(object):
-    def __init__(self, schema = {}):
+
+
+class Config(object):
+    def __init__(self, filepath, schema={}):
         self._schema = schema
+        self._filepath = filepath
+        self.config_data = OrderedDict()
+
+    def file_exists(self):
+        if os.path.isdir(self._filepath):
+            raise RuntimeError("A directory exists with file path '{0}'. "
+                               "Please remove or rename it.".format(self._filepath))
+
+        return os.path.isfile(self._filepath)
 
     # While loading the file, we do basic verification:
     # 1) Does the loaded file contain all the keys from the schema file?
     # 2) Are the values from the loaded file the same type as in the schema file?
     # At this point, only a shallow verification can be done, which means that
     # the content of lists or dictionaries will not be verified against the schema.
-    def load_file(self, fp):
-        data = yaml.load(fp, Loader=Ordered_Loader)
-        self.verify_data(data)
+    def load_file(self, verify=True):
+        with open(self._filepath, "r") as f:
+            data = yaml.load(f, Loader=Ordered_Loader)
 
-        # If this point is reached, that means no
-        # exceptions were raised.
-        return data
+            if verify:
+                self.verify_data(data)
 
-    def verify_data(self, data, schema = None, parent = ""):
-        print(parent)
+            # If this point is reached, that means no
+            # exceptions were raised.
+            self.config_data = data
+
+    def save_file(self, fp, verify=True):
+        if verify:
+            self.verify_data(self.config_data)
+
+        yaml.dump(self.config_data, Dumper=Ordered_Dumper)
+
+    def verify_data(self, data, schema=None, parent=""):
         if schema is None:
             schema = self._schema
 
@@ -56,28 +76,39 @@ class ConfigSchema(object):
 
         return True
 
-    def save_default(self, fp):
-        # We
-        yaml.dump(self._schema, fp,
-                  Dumper=Ordered_Dumper,
-                  default_flow_style=False,
-                  indent=4)
+    def save_default(self, fp=None):
+        with open(self._filepath, "w") as f:
+            yaml.dump(self._schema, f,
+                      Dumper=Ordered_Dumper,
+                      default_flow_style=False,
+                      indent=4)
 
+    # To provide an easier way to access information from inside the
+    # config data, we implement a few methods so that the Config object
+    # can be used a bit like a dict.
+    def __contains__(self, item):
+        return item in self.config_data
 
+    def __getitem__(self, key):
+        return self.config_data[key]
+
+    def __iter__(self):
+        return iter(self.config_data)
+
+    def __str__(self):
+        return str(self.config_data)
 
 
 if __name__ == "__main__":
     from misc.config_templates import default_bot_config
-    schema = ConfigSchema(default_bot_config)
-    print(default_bot_config)
 
-    with open("config.yaml", "w") as f:
-        schema.save_default(f)
-        #schema.load_file(f)
-    schema = ConfigSchema({})
-    with open("config_temp.yaml", "r") as f:
-        result = schema.load_file(f)
+    MyConfig = Config("config_temp.yaml", schema=default_bot_config)
 
-    print(result)
-    print(type(result))
-    print(default_bot_config == result)
+    if not MyConfig.file_exists():
+        MyConfig.save_default()
+        print("Created new file")
+
+    MyConfig.load_file()
+
+    print(MyConfig)
+    print(MyConfig["User Info"])
