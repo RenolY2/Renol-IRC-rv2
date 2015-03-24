4234de478e1fc2a9cdacde57d30ebe30f79a2e76
Yoshi2 <debugyoshi@gmail.com>
2015-03-24 15:41:58 +0100

    Add message handler; set irc_networking reference

--- irc_bot.py
+++ irc_bot.py
@@ -2,7 +2,10 @@ import time
 from collections import OrderedDict
 
 from core.networking import Networking
+from core.message_types.message_handler import MessageHandler
 from configuration import Config
+
+
 from misc.config_templates import default_bot_config
 
 
@@ -11,16 +14,20 @@ class IRCBot(object):
         self.bot_config = Config("config.yaml", schema=default_bot_config)
         self.bot_config.load_file()
 
+        conninfo = self.bot_config["Connection Info"]
+        self.irc_networking = Networking(conninfo["host"], conninfo["port"], timeout=conninfo["timeout"])
+        self._message_handler = MessageHandler()
+
     def start(self):
         userinfo = self.bot_config["User Info"]
         conninfo = self.bot_config["Connection Info"]
 
-        irc_networking = Networking(conninfo["host"], conninfo["port"], timeout=conninfo["timeout"])
-        irc_networking.start_threads()
 
-        irc_networking.send_msg("PASS", [userinfo["password"]])
-        irc_networking.send_msg("NICK", [userinfo["name"]])
-        irc_networking.send_msg("USER", [userinfo["ident"], "*", "*"], userinfo["realname"])
+        self.irc_networking.start_threads()
+
+        self.irc_networking.send_msg("PASS", [userinfo["password"]])
+        self.irc_networking.send_msg("NICK", [userinfo["name"]])
+        self.irc_networking.send_msg("USER", [userinfo["ident"], "*", "*"], userinfo["realname"])
 
         print("Read loop starting now!")
 
@@ -28,22 +35,22 @@ class IRCBot(object):
         running = True
 
         while running:
-            msg = irc_networking.read_msg()
+            msg = self.irc_networking.read_msg()
             if msg is None:
                 time.sleep(0.1)
             else:
                 pref, cmd, args, text = msg
                 print(msg)
                 if cmd == "PING":
-                    irc_networking.send_msg("PONG", message=text)
+                    self.irc_networking.send_msg("PONG", message=text)
                 if cmd == "004":
-                    irc_networking.send_msg("JOIN", ["#test"])
+                    self.irc_networking.send_msg("JOIN", ["#test"])
             """
             if (time.time() - last_ping) > 30:
                 IRCNetworking.send_msg("PING", [host])
                 last_ping = time.time()
             """
-            if irc_networking.thread_has_crashed:
+            if self.irc_networking.thread_has_crashed:
                 running = False
 
 if __name__ == "__main__":
