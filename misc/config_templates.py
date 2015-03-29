5f07aacbcef418feaba11caa43bffa6c2faed994
Yoshi2 <debugyoshi@gmail.com>
2015-03-29 20:35:52 +0200

    Add option to set channels on joining.
    Also start preparing for additional options.

--- misc/config_templates.py
+++ misc/config_templates.py
@@ -4,14 +4,33 @@ _user_info = OrderedDict([
     ("password", "abc"),
     ("name", "MyNickname"),
     ("ident", "RenolIRCBot"),
-    ("realname", "RenolIRCBot"),
+    ("realname", "RenolIRCBot")
 ])
 
 _connection_info = OrderedDict([
     ("host", ""),
     ("port", 6667),
     ("bind address", ["", 0]),
-    ("timeout", 300)
+    ("timeout", 300),
+    ("channels", [])
+])
+
+_message_sending = OrderedDict([
+    ("minimum delay", 2.0),
+
+    # This has an effect on how quickly the message
+    # delay rises for each message sent.
+    # A lower number means that the delay rises quicker,
+    # but the maximum delay will always be 2 seconds.
+    ("messages per minute", 30),
+
+    # This is for how many messages the minimum delay
+    # should be kept.
+    ("burst", 0)
+])
+
+_bot_options = OrderedDict([
+
 ])
 
 default_bot_config = OrderedDict([
