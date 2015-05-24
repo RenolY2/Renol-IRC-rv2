e8638bd1119c66e122f3a7768edc9b2aece7df41
Yoshi2 <debugyoshi@gmail.com>
2015-05-24 19:03:03 +0200

    Get rid of properties, change indentation

--- core/irc/irc_targets.py
+++ core/irc/irc_targets.py
@@ -9,9 +9,10 @@ class UserNotInChannelError(Exception):
     def __str__(self):
         return ("User '{name}'(lower case: {name_lower}) "
                 "doesn't exist in channel '{chan}'".format(
-            name=self.name, name_lower=self.name_lowercase,
-            chan=self.channel_name
-            ))
+                    name=self.name, name_lower=self.name_lowercase,
+                    chan=self.channel_name
+                    )
+        )
 
 
 class UserAlreadyInChannelError(Exception):
@@ -23,63 +24,41 @@ class UserAlreadyInChannelError(Exception):
     def __str__(self):
         return ("User '{name}'(lower case: {name_lower}) "
                 "already exists in channel '{chan}'".format(
-            name=self.name, name_lower=self.name_lowercase,
-            chan=self.channel_name
-        ))
+                    name=self.name, name_lower=self.name_lowercase,
+                    chan=self.channel_name
+                    )
+        )
 
 
 # An IRC target is an entity to which a private message can be sent.
 # The only IRC targets, as of now, are the IRC user and the IRC channel.
 class IRCTarget(object):
     def __init__(self, name):
-        self._name = name
-
-    @property
-    def name(self):
-        return self._name
+        self.name = name
 
 
 
 class User(IRCTarget):
     # The irc server can set various flags on the user that
     # highlight his abilities.
-    _flags = {}
+    flags = {}
 
     # The ident and hostname of an user is not known when a channel
     # is first joined. This information needs to be added
     # at a later time, e.g. by sending a WHO command and parsing the response.
-    _identity = None
-    _hostname = None
+    identity = None
+    hostname = None
 
 
-    _authed = False
+    authed = False
 
     def __init__(self, name):
         super().__init__(name)
 
 
-    def _change_flag(self, flag):
-        self.flag =flag
-
-    def _change_nick(self, nickname):
-        self._name = nickname
-
-    @property
-    def flag(self):
-        return self._flag
-
-    @property
-    def identity(self):
-        return self._identity
-
-    @property
-    def hostname(self):
-        return self._hostname
-
-
 class Channel(IRCTarget):
-    _topic = ""
-    _userlist = {}
+    topic = ""
+    userlist = {}
 
     def __init__(self, name, lowercase_func, uppercase_func):
         super().__init__(name)
@@ -87,16 +66,8 @@ class Channel(IRCTarget):
         self._lowercase_func = lowercase_func
         self._uppercase_func = uppercase_func
 
-    def _set_topic(self, topic):
+    def set_topic(self, topic):
         pass
-
-    @property
-    def topic(self):
-        return self._topic
-
-    @property
-    def userlist(self):
-        return self._userlist
 
     # Add the user to the channel's userlist. The user must not exist yet.
     def add_user(self, name, user_obj):
@@ -105,14 +76,14 @@ class Channel(IRCTarget):
         if name_lower in self._userlist:
             raise UserAlreadyInChannelError()
 
-        self._userlist[name_lower] = user_obj
+        self.userlist[name_lower] = user_obj
 
     # Remove the user from the channel's userlist. The user must exist.
     def remove_user(self, name):
         name_lower = self._lowercase_func(name)
 
         try:
-            del self._userlist[name_lower]
+            del self.userlist[name_lower]
         except KeyError:
             raise UserNotInChannelError(name, name_lower, self.name)
 
@@ -121,11 +92,11 @@ class Channel(IRCTarget):
         name_lower = self._lowercase_func(name)
 
         try:
-            return self._userlist[name_lower]
+            return self.userlist[name_lower]
         except KeyError:
             raise UserNotInChannelError(name, name_lower, self.name)
 
     def user_exists(self, name):
         name_lower = self._lowercase_func(name)
 
-        return name_lower in self._userlist
+        return name_lower in self.userlist
