8a5eb1ce8962ee94d15f3c0be6f5201647523c4c
Yoshi2 <debugyoshi@gmail.com>
2015-05-27 11:26:42 +0200

    Modify init to set the message handlers

--- core/message_types/irc_module_template.py
+++ core/message_types/irc_module_template.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractmethod
 
 class IRCModule(ABC):
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.set_message_handlers(self.bot.message_handler.set_handler)
 
     @abstractmethod
     def set_message_handlers(self, set_handler):
